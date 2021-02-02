ef95592bbe996c42549ba52fb8fba773cc5327e3
Roman Hushpit <roman.hushpit@synapse.com>
2021-02-02 22:09:17 +0200

    Initial commit

---
+++ src/test/java/com/roman/awsintegration/AwsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.awsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
