e7258458a9feffaf81124b34a68e09103b854129
roman hushpit <roman.hushpit@globallogic.com>
2021-02-20 16:56:46 +0200

    Last update

--- src/main/java/com/roman/awsintegration/health/ServiceHealthIndicator.java
+++ src/main/java/com/roman/awsintegration/health/ServiceHealthIndicator.java
@@ -1,12 +1,15 @@
 package com.roman.awsintegration.health;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceHealthIndicator implements HealthIndicator {
-
+    @Autowired
+    private BuildProperties buildProperties;
 
     private final String message_key = "Service A";
 
@@ -15,7 +18,12 @@ public class ServiceHealthIndicator implements HealthIndicator {
         if (!isRunningServiceA()) {
             return Health.down().withDetail(message_key, "Not Available").build();
         }
-        return Health.up().withDetail(message_key, "Available").build();
+        return Health.up()
+                .withDetail(message_key, "Available")
+                .withDetail("name", buildProperties.getName())
+                .withDetail("version", buildProperties.getVersion())
+                .withDetail("deployment date", buildProperties.getTime().toString())
+                .build();
     }
 
     private Boolean isRunningServiceA() {
